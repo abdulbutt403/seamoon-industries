4593f6d7feab53272f810679d3a895f9b3b89863
abdulbutt403 <75443479+abdulbutt403@users.noreply.github.com>
2022-10-03 08:19:32 +0500

    ui changes

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -18,7 +18,35 @@ const CardDeal = () => (
     </div>
 
     <div className={layout.sectionImg}>
-      <img src={card} alt="billing" className="w-[100%] h-[100%]" />
+    <section class="app" id="app" data-current-media="book">
+    
+    <article class="media-container">
+        <div class="book-wrapper">
+            <div class="book">
+                <div class="book__front">
+                    <img src="https://images-na.ssl-images-amazon.com/images/I/91-j2UzZW4L.jpg" alt="cover"/>
+                </div>
+                <div class="book__paper"></div>
+                <div class="book__back"></div>
+            </div>
+            <div class="book-shadow"></div>
+        </div>
+        
+        <div class="movie-wrapper">
+            <div class="movie">
+                <div class="movie__front">
+                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/184729/2019-Annihilation-DVD-and-Bluray-Cover-UK-and-USA-CST1.jpg" alt="cover"/>
+                </div>
+                <div class="movie__edge"></div>
+                <div class="movie__side"></div>
+            </div>
+            <div class="movie-shadow"></div>
+        </div>
+    </article>
+    
+  
+    
+</section>
     </div>
   </section>
 );
--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -2,15 +2,37 @@ import { clients } from "../constants";
 import styles from "../style";
 
 const Clients = () => (
-  <section className={`${styles.flexCenter} my-4`}>
-    <div className={`${styles.flexCenter} flex-wrap w-full`}>
-      {clients.map((client) => (
-        <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px] m-5`}>
-          <img src={client.logo} alt="client_logo" className="sm:w-[192px] w-[100px] object-contain" />
+  <section class="app" id="app" data-current-media="book">
+    
+    <article class="media-container">
+        <div class="book-wrapper">
+            <div class="book">
+                <div class="book__front">
+                    <img src="https://images-na.ssl-images-amazon.com/images/I/91-j2UzZW4L.jpg" alt="cover"/>
+                </div>
+                <div class="book__paper"></div>
+                <div class="book__back"></div>
+            </div>
+            <div class="book-shadow"></div>
         </div>
-      ))}
-    </div>
-  </section>
+        
+        <div class="movie-wrapper">
+            <div class="movie">
+                <div class="movie__front">
+                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/184729/2019-Annihilation-DVD-and-Bluray-Cover-UK-and-USA-CST1.jpg" alt="cover"/>
+                </div>
+                <div class="movie__edge"></div>
+                <div class="movie__side"></div>
+            </div>
+            <div class="movie-shadow"></div>
+        </div>
+    </article>
+    
+  
+    
+</section>
+
+
 );
 
 export default Clients;
