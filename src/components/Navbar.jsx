1735c21e437045bd81cc70925cf4c0313460c2fd
abdulbutt403 <75443479+abdulbutt403@users.noreply.github.com>
2022-11-01 14:39:27 +0500

    margin 28

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
           </li>
         ))}
         <li
-        style={{marginLeft: 10}}
+        style={{marginLeft: 28}}
           className={`font-poppins font-medium cursor-pointer text-[16px] ${"text-white"}`}
         >
           <a
